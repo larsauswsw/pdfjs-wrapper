f7f429b23fa8ec306f87aa76cc907e4a85915154
Florian Heeg <florian.heeg@etecture.de>
2014-02-25 13:54:53 +0100

    jshint and watch task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,24 @@
-'use strict';
-module.exports= function(grunt) {
-	// show elapsed time at the end
-	require('time-grunt')(grunt);
-	// load all grunt tasks
-	require('load-grunt-tasks')(grunt);
-	// Project configuration.
-	grunt.initConfig();
-	
-	grunt.registerTask('default', function() {
-		return true;
-	});
+module.exports = function(grunt) {'use strict';
+
+    // show elapsed time at the end
+    require('time-grunt')(grunt);
+    // load all grunt tasks
+    require('load-grunt-tasks')(grunt);
+    // Project configuration.
+    grunt.initConfig({
+        watch : {
+            scripts : {
+                files : ['**/*.js'],
+                tasks : ['jshint'],
+                options : {
+                    spawn : false
+                }
+            }
+        },
+        jshint : {
+            all : ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js']
+        }
+    });
+
+    grunt.registerTask('default', ['jshint', 'watch']);
 };
